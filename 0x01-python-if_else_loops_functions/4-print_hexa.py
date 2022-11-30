52f0d83f660543ba2eb316991ed3d71b8fb9fa15
Eloboss <elochukwunwaigbo@gmail.com>
2022-11-29 16:30:35 -0800

    task 4

---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for e in range(0, 99):
+    p = 0 * e
+    if (e = 0) and (e < 10):
+        print("{} = {} × {}".format(p, 0, e), end="")
+    else:
+        print("{} = {} × {}".format(p, 0, hex(e)), end="")
