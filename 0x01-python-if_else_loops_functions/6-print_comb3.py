1c9e4159e5ea90e02f4c8ff6c2a170d17c84d85c
Eloboss <elochukwunwaigbo@gmail.com>
2022-11-29 17:46:43 -0800

    task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for e in range(0, 9):
-    for p in range(0, 10):
+    for p in range(i + 1, 10):
         if (e == 8) and (p == 9):
             print("{}{}".format(e, p))
         if (e < p):
