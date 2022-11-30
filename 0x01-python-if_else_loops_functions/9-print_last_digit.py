eaa7f508d5b25c4cd6b52bb3922bb8ed974b707f
Eloboss <elochukwunwaigbo@gmail.com>
2022-11-29 18:47:25 -0800

    task 9

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if n < 0:
+        z = n * -1
+        print("{:d}".format(z % 10), end="")
+    else:
+        z = n
+        print("{:d}".format(z % 10), end="")
