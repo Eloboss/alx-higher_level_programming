3dcc6ef83bbc45e86173fcb6252c1c9e9ca438ce
Eloboss <elochukwunwaigbo@gmail.com>
2022-12-01 11:03:27 -0800

    task 0

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """print the result of addition"""
+    from add_0 import add
+    a = 1
+    b = 2
+    sum = a + b
+
+    print("{} + {} = {}".format(a, b, sum)
