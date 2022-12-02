79e9a71ab4995aa82dc3c4d468a406a188f9b872
Eloboss <elochukwunwaigbo@gmail.com>
2022-12-02 15:25:29 -0800

    task 4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    i = my_list.copy()
+    elo = len(i)
+    boss = i.insert(idx, element)
+    if idx< 0 or idx > eli:
+        return i
+    else:
+        return boss
