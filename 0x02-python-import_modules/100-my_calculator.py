e7e9265ab831f572881fe3a88daeaedd80fca2ac
Eloboss <elochukwunwaigbo@gmail.com>
2022-12-01 16:20:14 -0800

    task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,19 +4,19 @@ if __name__ == "__main__":
     import sys
     boss = len(sys.argv)
     if boss != 4:
-        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     a = int(sys.argv[1])
     op = sys.argv[2]
     b = int(sys.argv[3])
-    if op is "+":
-        print("{} {} {} = {}".format(a, b , add(a, b)))
-    elif op is "-":
-        print("{} {} {} = {}".format(a, b , sub(a, b)))
-    elif op is "*":
-        print("{} {} {} = {}".format(a, b , mul(a, b)))
-    elif op is "/":
-        print("{} {} {} = {}".format(a, b , div(a, b)))
+    if op is '+':
+        print("{} {} {} = {}".format(a, b, add(a, b)))
+    elif op is '-':
+        print("{} {} {} = {}".format(a, b, sub(a, b)))
+    elif op is '*':
+        print("{} {} {} = {}".format(a, b, mul(a, b)))
+    elif op is '/':
+        print("{} {} {} = {}".format(a, b, div(a, b)))
     else:
-        print('Unknown operator. Available operators: +, -, * and /')
+        print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
