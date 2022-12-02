09509767665443c0ffbae729d5b69d1380db7d47
Eloboss <elochukwunwaigbo@gmail.com>
2022-12-02 15:02:00 -0800

    task 2

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    boss = len(my_list)
+    i = my_list.insert(idx, element)
+    if idx < 0 or idx > boss:
+        return my_list
+    else:
+        return i
