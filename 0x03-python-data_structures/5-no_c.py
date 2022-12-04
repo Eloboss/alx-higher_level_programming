9c07f0d77aeb2324c01356da3a573cc4b6ffa052
Eloboss <elochukwunwaigbo@gmail.com>
2022-12-04 14:19:48 -0800

    task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     boss = ""
-    for i in my string:
+    for i in my_string:
         if i != 'c' or i != 'C':
             boss += i
         print(boss)
