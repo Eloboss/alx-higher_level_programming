e7f9a484df5b26c796e2bf673a273433c3708098
Eloboss <elochukwunwaigbo@gmail.com>
2022-12-04 14:56:38 -0800

    task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,9 +3,10 @@ def max_integer(my_list=[]):
     boss = len(my_list)
     if boss == 0:
         return None
-    max = my_list[0]
-    for elo in range(boss):
-        if my_list[i] > max:
-            max = elo
-            return elo
-    return max
+    else:
+        max = my_list[0]
+        for elo in range(boss):
+            if my_list[i] > max:
+                max = elo
+                return elo
+        return max
