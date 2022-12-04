43f368e203a2fd37078acc86c1161f2ad4a9cc56
Eloboss <elochukwunwaigbo@gmail.com>
2022-12-04 13:47:09 -0800

    task 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    i = my_list.reverse()
-    for boss in i:
-        print("{:d}".format(boss))
+    boss = reversed(my_list)
+    print("{:d}".format(boss))
