eb4fe30055000abecc902d954c977f0f4ba6dca4
Eloboss <elochukwunwaigbo@gmail.com>
2022-12-04 13:23:59 -0800

    task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,7 +2,7 @@
 def replace_in_list(my_list, idx, element):
     boss = len(my_list)
     i = my_list.insert(idx, element)
-    if idx < 0 or idx > boss:
+    if idx < 0 or idx > boss - 1:
         return my_list
     else:
         return i
