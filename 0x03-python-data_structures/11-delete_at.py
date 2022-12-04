b9f177904bf956cbe5d3996dc4402409c63f023b
Eloboss <elochukwunwaigbo@gmail.com>
2022-12-04 15:19:19 -0800

    task 11

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,7 +2,7 @@
 def delete_at(my_list=[], idx=0):
     boss = len(my_list)
     elo = del my_list[idx]
-    if idx< 0 or idx > boss:
+    if idx < 0 or idx > boss - 1:
         return my_list
     else:
         return elo
