343c8bbc4e6d4bc3e45c764a300ca33ae68de0b6
Eloboss <elochukwunwaigbo@gmail.com>
2022-12-04 15:37:34 -0800

    ,task 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    elo = reversed(my_list)
+    elo = my_list.reverse()
     for bossu in elo:
         print("{:d}".format(bossu))
