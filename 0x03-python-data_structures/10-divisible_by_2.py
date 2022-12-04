c6f3bbf6706ed4362e1db334bddc0003078561f6
Eloboss <elochukwunwaigbo@gmail.com>
2022-12-04 15:16:42 -0800

    task 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,6 +3,7 @@ def divisible_by_2(my_list=[]):
     elo = []
     for i in my_list:
         if i % 2 == 0:
-            return elo.append(True)
+            elo.append(True)
         else:
-            return elo.append(False)
+            elo.append(False)
+    return elo
