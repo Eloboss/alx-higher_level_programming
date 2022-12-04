b7d183f0e1cba5392f252a34a233c1552fa739df
Eloboss <elochukwunwaigbo@gmail.com>
2022-12-04 13:21:31 -0800

    task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 def element_at(my_list, idx):
     boss = len(my_list)
     for i in my_list:
-        if idx < 0 or idx > boss:
+        if idx < 0 or idx > boss - 1:
             return None
         else:
             return my_list[idx]
