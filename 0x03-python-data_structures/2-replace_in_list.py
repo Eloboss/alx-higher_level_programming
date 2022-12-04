dd9f874a57140cea2406307d9274ca621c10cb7a
Eloboss <elochukwunwaigbo@gmail.com>
2022-12-04 13:31:46 -0800

    task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     boss = len(my_list)
-    i = my_list.insert(idx, element)
     if idx < 0 or idx > boss - 1:
         return my_list
     else:
-        return i
+        my_list[idx] = elo
+        return my_list
