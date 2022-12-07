90834a9ba244d88fff3238a67b9084c75aec182d
Eloboss <elochukwunwaigbo@gmail.com>
2022-12-07 15:20:54 -0800

    task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    x = [[e ** 2 for e in row] for row in matrix]
-    return x
+    elo = [[e ** 2 for e in row] for row in matrix]
+    return elo
