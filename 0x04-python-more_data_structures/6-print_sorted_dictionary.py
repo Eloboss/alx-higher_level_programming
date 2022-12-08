2b3ae4a92bb4045023625246db5bd739455b04ce
Eloboss <elochukwunwaigbo@gmail.com>
2022-12-07 17:44:19 -0800

    task 6

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     for elo in sorted(a_dictionary):
-        print("{:s} : {}".format(elo, a_dictionary[elo]))
+        print("{} : {}".format(elo, a_dictionary[elo]))
