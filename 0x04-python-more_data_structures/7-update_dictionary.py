d834ebacf5df7c24f8f7caef8e98fabc83774077
Eloboss <elochukwunwaigbo@gmail.com>
2022-12-07 17:51:33 -0800

    task 7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -6,4 +6,4 @@ def update_dictionary(a_dictionary, key, value):
         for elo in a_dictionary:
             if elo == key:
                 a_dictionary[elo] = value
-                return a_dictionary
+    return a_dictionary
