0211d94fffd2aca6f460b4eafa18fdb366f4663f
Eloboss <elochukwunwaigbo@gmail.com>
2022-12-07 17:12:41 -0800

    task 15

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,6 +2,6 @@
 def complex_delete(a_dictionary, value):
     elo = list(a_dictionary.keys())
     for boss in elo:
-        if value == a_dictionary.get(boss):
+        if a_dictionary.get(boss) == value:
             del a_dictionary[boss]
-    return a_dictionary
+            return a_dictionary
