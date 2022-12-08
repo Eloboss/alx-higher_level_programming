2f58d9d26e3e694c6ea2eb416b9876d3e37b64af
Eloboss <elochukwunwaigbo@gmail.com>
2022-12-07 17:28:48 -0800

    task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    pen = []
     boss = len(my_list)
     for elo in range(boss):
         if my_list[elo] == search:
-            my_list.append(replace)
-            return my_list
+            pen.append(replace)
+            return pen
+        else:
+            pen.append(my_list[elo])
+            return pen
