768f23c7242c974b40262d08ded8e130a12ebc2e
Eloboss <elochukwunwaigbo@gmail.com>
2022-12-19 12:54:47 -0800

    task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        int(a / b)
-        sum = int(a / b)
-    except ZeroError:
+        sum = (a / b)
+    except (ZeroDivisionError, TypeError):
         sum = None
     finally:
         print("{}".format(sum))
+    return (sum)
