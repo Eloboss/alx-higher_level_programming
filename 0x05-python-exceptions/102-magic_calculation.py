68735b0bf018fc806234711f679fff34cdae77fc
Eloboss <elochukwunwaigbo@gmail.com>
2022-12-19 12:30:28 -0800

    task 9

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    try:
-        result = 0
+    result = 0
         for i in range(1, 3):
-            if (i > a):
-                raise Exception('Too far')
-            result += a ** b / i
-    except Exception:
-        result = b + a
-        break
-    return(result)
+            try:
+                if (i > a):
+                    raise Exception('Too far')
+                result += a ** b / i
+            except Exception:
+                result = b + a
+                break
+        return(result)
