7edc15dd2f77b54f4f3834c7aebba35d722cd90c
Eloboss <elochukwunwaigbo@gmail.com>
2022-12-19 16:40:16 -0800

    task 7

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    def safe_print_integer_err(value):
-        try:
-            print("{:d}".format(value))
-            return (True)
-        except (TypeError, ValueError):
-            print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-            return (False)
+
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except (TypeError, ValueError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (False)
