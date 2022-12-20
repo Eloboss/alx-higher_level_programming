d5a24c5bf2d4e7664f12212128a7a191c87a114f
Eloboss <elochukwunwaigbo@gmail.com>
2022-12-20 14:19:35 -0800

    task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -11,6 +11,7 @@ class Square:
         """
         if type(size) is not int:
             raise TypeError("size must be an integer")
+
         if size < 0:
             raise ValueError("size must be >= 0")
 
