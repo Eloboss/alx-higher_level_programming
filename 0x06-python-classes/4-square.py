e5440f9f4d8fee0924b81e6661d9ed74fa0a4633
Eloboss <elochukwunwaigbo@gmail.com>
2022-12-20 14:51:39 -0800

    task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Define a square."""
+
+
+class Square:
+    """Represent a square."""
+    def __init___(self, size=0):
+        """Square with attributes
+        args:
+            size: length of a square
+        """
+        self.size = size
+    @property
+    def size(self):
+    """To retrieve d size."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+    """To set d attributes"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """Returns area of square."""
+        return self.__size * self.__size
