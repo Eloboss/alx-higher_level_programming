ea4a7e6cc2a246bc7b6762c6a6e1ad7992621f8a
Eloboss <elochukwunwaigbo@gmail.com>
2022-12-19 16:14:33 -0800

    task 7

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -2,7 +2,7 @@
 def safe_print_integer_err(value):
     import sys
     try:
-        print("{:d}".format(value)
+        print("{:d}".format(value))
         return ('True')
     except BaseException as e:
         print("Exception: {}".format(x), file=sys.stderr)
