75cec7476c8c6cb6a192129e9856f1f3bebbffc6
Eloboss <elochukwunwaigbo@gmail.com>
2022-12-20 04:43:22 -0800

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/puthon3
+"""Represent a square."""
+
+
+class Square:
+    """Define asquare."""
+    def __init__(self, __size):
+        """Square with attrubutes."""
+        self.__size = size
