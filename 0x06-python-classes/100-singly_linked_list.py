daa5393990dceaed131bb9cb4a4c043c79389305
Eloboss <elochukwunwaigbo@gmail.com>
2022-12-20 18:35:34 -0800

    task 7

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -33,7 +33,7 @@ class Node:
     @next_node.setter
     def next_node(self, value):
         """Sets d node attributes"""
-        if value is not None and value is not Node:
+        if value is not None and type(value) is not Node:
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
