77f698d51824deb23bc617e8d984cf3d937837c0
Eloboss <elochukwunwaigbo@gmail.com>
2023-01-02 16:05:17 -0800

    task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -27,8 +27,7 @@ class Rectangle:
         if value < 0:
             raise ValueError('width must be >= 0')
 
-        else:
-            return self.__width = value
+        self.__width = value
 
     @property:
     def height(self):
@@ -44,5 +43,4 @@ class Rectangle:
         if value < 0:
             raise ValueError('height must be >= 0')
 
-        else:
-            return self.__height = value
+        self.__height = value
