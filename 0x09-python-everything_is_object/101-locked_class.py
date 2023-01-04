f4d93c797840e70c5e580f60d8bc3d98e7473ffe
Eloboss <elochukwunwaigbo@gmail.com>
2023-01-03 18:08:16 -0800

    task 30

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -3,4 +3,13 @@
 
 class LockedClass:
     """To detect errors"""
-     __slots__ = ["first_name"]
+    def __setattr__(self, name, value):
+        """locked class with attributes"""
+        if name != "first_name":
+            raise AttributeError("'LockedClass' object has no attribute '" +
+                                 name + "'")
+
+    def __getattribute__(self, name):
+        if name == "__dict__":
+            raise AttributeError("'LockedClass' object has no attribute '" +
+                                 name + "'")
