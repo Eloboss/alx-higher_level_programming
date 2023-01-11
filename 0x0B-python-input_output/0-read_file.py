c668888ce2abc461e29d892b72ad471dac0cb786
Eloboss <elochukwunwaigbo@gmail.com>
2023-01-10 17:42:52 -0800

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,5 +2,5 @@
 
 
 def read_file(filename=""):
-    with open(filename, "r", encoding="utf-8") as elo:
-        print(elo.read(), end="")
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
