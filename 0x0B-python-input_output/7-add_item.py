dcf4e1b54f387459d764325f40ed38b7a1681998
Eloboss <elochukwunwaigbo@gmail.com>
2023-01-10 18:44:23 -0800

    task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""adds all list to a file"""
+
+
+import sys
+
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+
+    try:
+        boss = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        boss = []
+    boss.extend(sys.argv[1:])
+    save_to_json_file(boss, "add_item.json")
