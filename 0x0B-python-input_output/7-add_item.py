dda76b08a3c009f1132935c238a585808167e81b
Eloboss <elochukwunwaigbo@gmail.com>
2023-01-10 19:51:06 -0800

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
-"""adds all list to a file"""
-
-
+"""Add all arguments to a Python list and save them to a file."""
 import sys
-import json
-
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_json = __import__('6-load_from_json_file').load_from_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
     try:
-        boss = load_json("add_item.json")
+        items = load_from_json_file("add_item.json")
     except FileNotFoundError:
-        boss = []
-    boss.append(sys.argv[1:])
-    save_to_json_file(boss, "add_item.json")
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
