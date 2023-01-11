c1f6446037ed443de094fb141598e03bfb62dd45
Eloboss <elochukwunwaigbo@gmail.com>
2023-01-10 17:57:54 -0800

    task 2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""append a file"""
+
+
+def append_write(filename="", text=""):
+    """returns an appended file"""
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.append(text)
