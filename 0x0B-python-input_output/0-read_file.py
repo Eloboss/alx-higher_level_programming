51d2b6ea37a76e310981ed3c40560e2a56a6681f
Eloboss <elochukwunwaigbo@gmail.com>
2023-01-10 17:45:09 -0800

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""input and otput file"""
 
 
 def read_file(filename=""):
+    """prints a read file"""
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
