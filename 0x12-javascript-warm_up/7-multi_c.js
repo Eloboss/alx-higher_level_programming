574e681d6d9f7606c1e8b1866c217f7c276cce33
Eloboss <elochukwunwaigbo@gmail.com>
2023-03-13 19:54:40 -0700

    ,task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,7 +2,7 @@
 const integer = parseInt(process.argv[2]);
 if (integer) {
   for (let i = 0; i < integer; i++) {
-  console.log('C is fun');
+    console.log('C is fun');
   }
 } else {
   console.log('Missing number of occurrences');
