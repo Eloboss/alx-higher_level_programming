f73f4c5d43f23a2ca140e4801bf4bcc6c94cfa0a
Eloboss <elochukwunwaigbo@gmail.com>
2023-03-13 20:23:17 -0700

    task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const langs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (const lan of langs) {
-  console.log(langs[lan]);
+for (const lane of langs) {
+  console.log(langs[lane]);
 }
