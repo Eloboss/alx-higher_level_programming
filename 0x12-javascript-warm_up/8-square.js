f27c98975f4a4604f4adbe5cdd79815b7187c1c6
Eloboss <elochukwunwaigbo@gmail.com>
2023-03-13 19:57:37 -0700

    ,task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const size = parseInt(process.argv[2]);
 if (size >= 0) {
- for (let i = 0; i < size; i++) {
+  for (let i = 0; i < size; i++) {
     console.log('X'.repeat(size));
   }
 } else {
