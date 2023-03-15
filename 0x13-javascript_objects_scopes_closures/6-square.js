cc1a221237e0346f88caec38f60e727929341960
Eloboss <elochukwunwaigbo@gmail.com>
2023-03-14 19:30:36 -0700

    task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const EloBoss = require('./5-square.js');
+module.exports = class Square extends EloBoss {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+      for (let i = 0; i < this.height; i++) {
+        console.log('c'.repeat(this.width));
+      }
+    }
+  }
+};
