bc6de47e9e39842de47efd4267399a40cffd3fbc
Eloboss <elochukwunwaigbo@gmail.com>
2023-03-14 20:55:36 -0700

    task 12

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const dict = require('./101-data').dict;
+const newdict = {};
+for (const uid in dict) {
+  if (newdict[dict[uid]] === undefined) {
+    newdict[dict[uid]] = [];
+  }
+  newdict[dict[uid]].push(uid);
+}
+
+console.log(newdict);
