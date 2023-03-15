e58e08b90ac5b0d3966cedba9e5df0081813c80b
Eloboss <elochukwunwaigbo@gmail.com>
2023-03-14 21:04:43 -0700

    ,task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -3,7 +3,7 @@ exports.esrever = function (list) {
   const array = [];
   let elo = 0;
   for (let boss = list.length - 1; boss >= 0; boss--, elo++) {
-    array[elo] = array[boss];
+    array[elo] = list[boss];
   }
   return array[elo];
 };
