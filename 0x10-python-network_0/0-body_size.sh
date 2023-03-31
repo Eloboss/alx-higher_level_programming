fc92df60c82bc3c7d043fc291bc29785a72c06f0
Eloboss <elochukwunwaigbo@gmail.com>
2023-03-30 18:56:56 -0700

    task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#script that takes in a URL and display size of the message body
+# script that takes in a URL and display size of the message body
 curl -sI "$1" | grep -E 'Content-Length: [0-9]+' | cut -d " " -f2
-
