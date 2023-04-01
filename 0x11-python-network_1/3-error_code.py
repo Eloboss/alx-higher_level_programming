79f1e9e1c4ef68bd0e5c4ea2c409bd8b40247aae
Eloboss <elochukwunwaigbo@gmail.com>
2023-03-31 19:27:35 -0700

    task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Script that takes url and prints error if exists"""
+"""
+Script that takes url and prints error if exists
+"""
 
 
 import urllib.request
@@ -8,7 +10,7 @@ import sys
 
 if __name__ == "__main__":
     try:
-       with urllib.request.urlopen(sys.argv[1]) as elo:
-           print('elo.read().decode("utf-8", "decode"'))
-    except as urllib.error.HTTPErtor as boss:
-        print('Error code: {}'.format(boss.code))
+        with urllib.request.urlopen(sys.argv[1]) as elo:
+            print(elo.read().decode("utf-8", "decode"))
+    except as urllib.error.HTTPError as boss:
+        print("Error code: {}".format(boss.code))
