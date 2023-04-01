f3fe1a4f2259b5d0cf91730144035a836d8afe75
Eloboss <elochukwunwaigbo@gmail.com>
2023-03-31 21:49:47 -0700

    task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
     import requests
 
     elo = requests.get('https://intranet.hbtn.io/status')
+
     print('Body response:')
     print('\t- type: {}'.format(type(elo.text)))
     print('\t- content: {}'.format(elo.text))
