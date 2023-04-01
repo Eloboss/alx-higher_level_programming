fef563599f2eda974f0920c7c581b7962b14dc11
Eloboss <elochukwunwaigbo@gmail.com>
2023-03-31 19:16:05 -0700

    task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-"""sends a request to the URL and displays a variable in the head"""
+"""
+sends a request to the URL and displays a variable in the head X-Request-id
+"""
 
 
-if __name == "__main__":
+if __name__ == "__main__":
     import urllib.request
     import sys
 
