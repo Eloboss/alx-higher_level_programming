55dc362a91a1eae8665f061e1e264582722f27a1
Eloboss <elochukwunwaigbo@gmail.com>
2023-03-31 18:48:05 -0700

    task 5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """Script that gets the header Variable"""
 
+
 import requests
 import sys
 
 if __name == "__main__":
     elo = requests.get(sys.argv[1])
-    boss = elo.headers['X-Request-Id']
+    boss = elo.headers.get('X-Request-Id')
     print(boss)
