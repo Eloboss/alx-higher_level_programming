d0283386ee92fb21166e57647310b70f90571aa1
Eloboss <elochukwunwaigbo@gmail.com>
2023-03-31 19:39:45 -0700

    task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
     import sys
     try:
         with urllib.request.urlopen(sys.argv[1]) as elo:
-            print(elo.read().decode("utf-8", "decode"))
+            print(elo.read().decode("utf-8"))
     except as urllib.error.HTTPError as boss:
         print("Error code: {}".format(boss.code))
