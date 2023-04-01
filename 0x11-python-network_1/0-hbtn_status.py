269eec7bd074bba2912af71faff932149ae5041f
Eloboss <elochukwunwaigbo@gmail.com>
2023-03-31 19:09:14 -0700

    ,task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-""" Method to fetch url """
+""" Method to fetch url  https://alx-intranet.hbtn.io/status"""
 
 
 if __name__ == "__main__":
     import urllib.request
 
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as elo:
-    boss = elo.read()
-    print('Body response:')
-    print('\t- type: {}'.format(type(boss)))
-    print('\t- content: {}'.format(boss))
-    print('\t- utf8 content: {}'.format(boss.decode("utf-8", "replace")))
+        boss = elo.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(boss)))
+        print('\t- content: {}'.format(boss))
+        print('\t- utf8 content: {}'.format(boss.decode("utf-8", "replace")))
