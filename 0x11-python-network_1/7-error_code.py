c28f1bde8a0bdec179f470df5a1d8f8d8f616d9c
Eloboss <elochukwunwaigbo@gmail.com>
2023-03-31 20:07:51 -0700

    task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """Script tgat takes url and gives an error code if exists"""
 
-import requests
-import sys
 
 if __name__ == "__main__":
+    import requests
+    import sys
+
     elo = requests.get(sys.argv[1])
     if elo.status_code >= 400:
         print('Error code: {}'.format(elo.status_code))
