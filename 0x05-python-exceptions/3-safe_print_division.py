4d458b8d030643e4f5c9c1713ab41819caaa7405
Eloboss <111271524+Eloboss@users.noreply.github.com>
2024-02-08 03:50:47 +0100

    Delete 0x05-python-exceptions directory

--- 0x05-python-exceptions/3-safe_print_division.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def safe_print_division(a, b):
-    try:
-        sum = (a / b)
-    except (ZeroDivisionError, TypeError):
-        sum = None
-    finally:
-        print("Inside result: {}".format(sum))
-    return (sum)
